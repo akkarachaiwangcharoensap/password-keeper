f13ca91b8e756d9bb5aefc4b7bff529adf92b28f
Akkarachai Wangcharoensap <akkarachaiwangcharoensap@Akkarachais-iMac.local>
2019-07-06 01:12:07 -0700

    Removed: unnecessary codes

--- bundle.js
+++ bundle.js
@@ -72,44 +72,6 @@ function loadPasswords (customStorage)
 		passwordComponent.create(key, value, $passwords, customStorage);
 	});
 }
-
-/**
- * Initialize fields
- */
-function initialize ()
-{
-	if (typeof(Storage) == 'undefined') {
-		throw "Sorry, it seems your browser does not support local storage.";
-	}
-
-	$('.password').each((i, password) => {
-		
-		var $password = $(password);
-
-		var $key = $password.find('#key');
-		var $value = $password.find('#value');
-		var $copy = $password.find('#copy');
-		var $generate = $password.find('#generate');
-
-		// On copy button clicked
-		$copy.click((e) => {
-			e.preventDefault();
-
-			$value.select();
-			document.execCommand('copy');
-		});
-
-		// On generate button clicked
-		$generate.click((e) => {
-			e.preventDefault();
-			$value.val(PasswordGenerator.generateCustomPassword());
-		});
-	});
-
-	$('#new').click(() => {
-		newPasswordField();
-	})
-}
 },{"./custom-storage":1,"./password-component":6,"./password-generator":7,"jquery":3}],3:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.4.1
